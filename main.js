dc464c7369d26c299e973677e6d4b2557b1dd8dc
julianapivaro <169061712+julianapivaro@users.noreply.github.com>
2024-05-14 09:50:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/images.jpg") {
+    myImage.setAttribute("src", "images/images2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/images.jpg");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+myButton.onclick = () => {
+    setUserName();
+  };
+  
+
+  function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Juliana loves you, ${myName}`;
+    }
+  }
+  
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Juliana loves you, ${storedName}`;
+  }
+  
